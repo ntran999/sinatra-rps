1781c95b3af3dbffbe794877fb4a3999a49ccf87
ntran999 <146213565+ntran999@users.noreply.github.com>
2023-10-17 15:51:59 +0000

    RPS

--- app.rb
+++ app.rb
@@ -2,8 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/paper") do
+  options = ["paper","scissors","rock"]
+  @player_2 = options.sample
+  if @player_2 == "paper"
+    @outcome = " They played Paper! <br><br> We tied!"
+  end
+  if @player_2 == "rock"
+    @outcome = " They played Rock! <br><br> We won!"
+  end
+  if @player_2 == "scissors"
+    @outcome = " They played Scissors! <br><br> We lost!"
+  end
+  erb(:paper)
+end
+
+get("/rock") do
+  options = ["paper","scissors","rock"]
+  @player_2 = options.sample
+  if @player_2 == "paper"
+    @outcome = " They played Paper! <br><br> We lost!"
+  end
+  if @player_2 == "rock"
+    @outcome = " They played Rock! <br><br> We tied!"
+  end
+  if @player_2 == "scissors"
+    @outcome = " They played Scissors! <br><br> We won!"
+  end
+  erb(:rock)
+end
+
+get("/scissors") do
+  options = ["paper","scissors","rock"]
+  @player_2 = options.sample
+  if @player_2 == "paper"
+    @outcome = " They played Paper! <br><br> We won!"
+  end
+  if @player_2 == "rock"
+    @outcome = " They played Rock! <br><br> We lost!"
+  end
+  if @player_2 == "scissors"
+    @outcome = " They played Scissors! <br><br> We tied!"
+  end
+  erb(:scissors)
 end
